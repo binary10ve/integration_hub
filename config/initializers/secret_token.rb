cead4f714ab87efcbd87738f3c379b1904a8d5f3
santu <santu@santu.(none)>
2013-04-09 23:00:24 +0530

    empty project created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntegrationHub::Application.config.secret_token = 'ad82330edf77c25f8cb0ff88b7972d414ca34f467bb634ed1997d31f5ddd5ccaf5ded5e697f3998d4698e7ef383e8c7c3b18d2f9381fb32dc85ce8aaac4d2962'
